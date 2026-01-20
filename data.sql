5c870e42188fecc70aa4241e201cd5f5d99e2260
inezakevin23 <k.ineza1@alustudent.com>
2026-01-20 12:25:19 +0000

    The Population Challenge

---
+++ data.sql
@@ -0,0 +1,17 @@
+INSERT INTO rider(rider_id, name, motorcycle_plate) VALUES
+(1, 'ishimwe', 'RAB 174A'),
+(2, 'iradukunda', 'RAB 134A'),
+(3, 'mugisha', 'RAC 128A');
+
+INSERT INTO passenger (passenger_id, name, phone) VALUES
+(01, 'kevin', '0780000000'),
+(02, 'shema', '0788000001'),
+(03, 'joseph', '0788000002');
+
+INSERT INTO trips (trips_id, rider_id, passenger_id, fare, trip_date) VALUES
+(001 ,1 ,01 ,2000 ,'2026-01-20'),
+(002 ,2 ,01 ,4000 ,'2026-01-25'),
+(003 ,3 ,02 ,1500 ,'2026-01-28'),
+(004 ,1 ,03 ,2500 ,'2026-02-02'),
+(005 ,2 ,02 ,3000 ,'2026-02-10');
+
